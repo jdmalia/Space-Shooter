0a7c16b719dd10d84bef5a2bb21f25bb6f2e1d07
Jason Malia <jmalia3@gatech.edu>
2016-09-15 22:38:20 -0400

    Create bolt prefab

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+	public float speed;
+
+	void Start()
+	{
+		Rigidbody rb = gameObject.GetComponent<Rigidbody> ();
+		rb.velocity = transform.forward * speed;
+	}
+}
