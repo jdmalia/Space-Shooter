0502d41a877eb6d936b2d947d4a0daac7f04ac54
Jason Malia <jmalia3@gatech.edu>
2017-07-30 18:53:04 -0400

    Add explosions to asteroid and player

--- Assets/Scripts/DestroyByContract.cs
+++ Assets/Scripts/DestroyByContract.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class DestroyByContract : MonoBehaviour
 {
+
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Boundary")
             return;
-        
+
+        Instantiate(explosion, transform.position, transform.rotation);
+
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+        }
+
         Destroy(other.gameObject);
         Destroy(gameObject);
     }
